95727d0303fc883dde65025fe935e15d0000db8a
Gabo1234567890 <gabonenov@gmail.com>
2025-12-08 03:35:36 +0200

    Created color values

---
+++ Sponti/composeApp/src/androidMain/kotlin/org/tues/sponti/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package org.tues.sponti.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary1 = Color(0xFF5F0A87)
+val Primary2 = Color(0xFFFCE762)
+val Secondary1 = Color(0xFFC589E8)
+val Base0 = Color(0xFFFBF9FA)
+val Base20 = Color(0xFFEDEDED)
+val Base40 = Color(0xFFCDCBCD)
+val Base60 = Color(0xFFA7A3A8)
+val Base80 = Color(0xFF686369)
+val Base100 = Color(0xFF0E0114)
+val Success = Color(0xFF14B853)
+val Warning = Color(0xFFFFA238)
+val Error = Color(0xFFD22106)
