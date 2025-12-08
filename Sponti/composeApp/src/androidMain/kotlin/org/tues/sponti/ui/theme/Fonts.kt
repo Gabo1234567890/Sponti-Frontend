79403d73585833ffe2c966d913b98be759486025
Gabo1234567890 <gabonenov@gmail.com>
2025-12-08 11:51:52 +0200

    Fonts loaded and text styles made

---
+++ Sponti/composeApp/src/androidMain/kotlin/org/tues/sponti/ui/theme/Fonts.kt
@@ -0,0 +1,8 @@
+package org.tues.sponti.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import org.tues.sponti.R
+
+val PrimaryFont = FontFamily(Font(R.font.anek_gujarati))
+val SecondaryFont = FontFamily(Font(R.font.comfortaa))
