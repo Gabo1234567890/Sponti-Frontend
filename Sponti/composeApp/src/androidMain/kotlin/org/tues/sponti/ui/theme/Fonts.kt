6c82c17f69183aa10bf4227ab2621009b7749aaa
Gabo1234567890 <gabonenov@gmail.com>
2025-12-08 12:20:01 +0200

    Imported fonts fixed

--- Sponti/composeApp/src/androidMain/kotlin/org/tues/sponti/ui/theme/Fonts.kt
+++ Sponti/composeApp/src/androidMain/kotlin/org/tues/sponti/ui/theme/Fonts.kt
@@ -2,7 +2,14 @@ package org.tues.sponti.ui.theme
 
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import org.tues.sponti.R
 
-val PrimaryFont = FontFamily(Font(R.font.anek_gujarati))
-val SecondaryFont = FontFamily(Font(R.font.comfortaa))
+val PrimaryFont = FontFamily(
+    Font(R.font.anek_gujarati_medium, weight = FontWeight.Medium),
+    Font(R.font.anek_gujarati_bold, weight = FontWeight.Bold)
+)
+val SecondaryFont = FontFamily(
+    Font(R.font.comfortaa_medium, weight = FontWeight.Medium),
+    Font(R.font.comfortaa_bold, weight = FontWeight.Bold)
+)
