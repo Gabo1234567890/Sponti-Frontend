2815403052575d44afa027a1953355948da11eb9
Gabo1234567890 <gabonenov@gmail.com>
2025-12-24 22:13:12 +0200

    Deleted unnecessary file

--- Sponti/composeApp/src/androidMain/kotlin/org/tues/sponti/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package org.tues.sponti.navigation
-
-sealed class Screen(val route: String, val protected: Boolean) {
-    object CreateAccount: Screen("create-account", false)
-    object Login: Screen("login", false)
-    object VerifyEmail: Screen("verify-email", false)
-    object ForgotPassword: Screen("forgot-password", false)
-}
